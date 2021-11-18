91b247265df0a943544a9fdb9b8af63e37cf9ca0
Mehmet <mehmetkusken@gmail.com>
2021-11-17 17:50:45 -0700

    added user login and session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            render json: user
+            render json: {user: UserSerializer.new(user), token: encode_token(user.id)}
         else
             render json: {error: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
         end
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params_permit(:username, :password, :email, :zipcode)
+        params.permit(:username, :password, :email, :zipcode)
     end
 end
