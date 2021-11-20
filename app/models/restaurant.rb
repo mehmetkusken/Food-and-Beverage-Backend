50827d2eaffce15e5c124e4cc14719766906968d
Mehmet <mehmetkusken@gmail.com>
2021-11-19 19:24:27 -0700

    made relationships between tables

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
+    has_many :comments
+    has_many :users, through: :comments
 
     def self.get_by_location(location)
         YelpSearch.new(location).to_restaurants
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+    has_many :comments
+    has_many :restaurants, through: :comments
     has_secure_password
 end
